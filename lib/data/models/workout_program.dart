7ac9ec5de52af7ceefd29ab82946d0a250ae07bf
Alisher <aisroilov1029@gmail.com>
2024-06-13 18:16:24 +0500

    hive helper class added

--- lib/data/models/workout_program.dart
+++ lib/data/models/workout_program.dart
@@ -1,5 +1,30 @@
-class WorkoutProgram{
-  final String id;
+import 'package:hive/hive.dart';
+import 'package:uuid/uuid.dart';
+
+part 'workout_program.g.dart';
+
+const uuid = Uuid();
+
+@HiveType(typeId: 1)
+class WorkoutProgram {
+  @HiveField(0)
   final String name;
-  WorkoutProgram(this.id, this.name);
+  @HiveField(1)
+  final String id;
+
+  WorkoutProgram(this.name, id) : id = id ?? uuid.v4();
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+    };
+  }
+
+  factory WorkoutProgram.fromMap(Map<String, dynamic> map) {
+    return WorkoutProgram(
+      map['id'] as String,
+      map['name'] as String,
+    );
+  }
 }
---
+++ lib/data/models/workout_program.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'workout_program.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class WorkoutProgramAdapter extends TypeAdapter<WorkoutProgram> {
+  @override
+  final int typeId = 1;
+
+  @override
+  WorkoutProgram read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return WorkoutProgram(
+      fields[0] as String,
+      fields[1] as dynamic,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, WorkoutProgram obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.id);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is WorkoutProgramAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
