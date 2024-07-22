d01c6455c2d2c22003033441e84fd7d177054ade
Alisher <aisroilov1029@gmail.com>
2024-07-22 17:41:57 +0500

    calendar initial setup

--- lib/calendar/calendar_screen.dart
+++ lib/calendar/calendar_screen.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class CalendarScreen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  final today = DateTime.now();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
   @override
   Widget build(BuildContext context) {
-    final today = DateTime.now();
-    return TableCalendar(
-      focusedDay: today,
-      firstDay: DateTime(2020),
-      lastDay: DateTime(2025),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: Column(
+        children: [
+          TableCalendar(
+            calendarFormat: _calendarFormat,
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            focusedDay: today,
+            firstDay: DateTime(2020),
+            lastDay: DateTime(2025),
+          ),
+          const SizedBox(height: 10),
+          const Card(
+            child: Text("halo"),
+          ),
+        ],
+      ),
     );
   }
 }
